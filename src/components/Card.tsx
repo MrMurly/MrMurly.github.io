967c9f3e5ddbc34cf8e773c0991fcc358a4ccf26
Gustav Melefors <gusme799@student.liu.se>
2025-03-04 18:01:51 +0100

    centered text in card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,7 +11,7 @@ export const Card = ({ title, description, tags, logo }: CardProps) => {
   return (
     <>
       <div className="rounded-md bg-cyan-900 flex flex-col justify-center p-5">
-        <h3 className=" text-3xl font-bold capitalize">{title}</h3>
+        <h3 className="text-center text-3xl font-bold capitalize">{title}</h3>
         <img className="object-fill w-50 h-50 self-center" src={logo} />
         <div className="flex flex-col gap-2 justify-center">
           <span className="max-w-100  font-extralight text-sm/snug">
