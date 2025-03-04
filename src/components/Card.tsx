e7e34ba57d45589774a6c30fb79b176c5e7220f6
Gustav Melefors <gusme799@student.liu.se>
2025-03-04 16:10:25 +0100

    styled Tag and Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,7 +3,7 @@ import { Tag, TagProps } from "./Tag";
 export interface CardProps {
   title: string;
   description: string;
-  tags: [TagProps] | undefined;
+  tags: Array<TagProps> | undefined;
   logo: string;
 }
 
@@ -11,11 +11,13 @@ export const Card = ({ title, description, tags, logo }: CardProps) => {
   return (
     <>
       <div className="rounded-md bg-cyan-900 flex flex-col justify-center p-5">
-        <h3>{title}</h3>
+        <h3 className="font-mono text-3xl font-bold capitalize">{title}</h3>
         <img className="object-fill w-50 h-50 self-center" src={logo} />
-        <div className="flex flex-col gap-2">
-          <span className="max-w-100">{description}</span>
-          <div className="flex flex-row flex-wrap max-w-100 justify-center gap-2">
+        <div className="flex flex-col gap-2 justify-center">
+          <span className="max-w-100 font-mono font-extralight text-sm/snug">
+            {description}
+          </span>
+          <div className="flex flex-row flex-wrap max-w-100 gap-2 justify-between">
             {tags?.map((tag, i) => <Tag {...tag} key={i} />)}
           </div>
         </div>
--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -5,7 +5,7 @@ export interface TagProps {
 export const Tag = ({ title }: TagProps) => {
   return (
     <div className="rounded-md bg-blue-400 p-1 px-3">
-      <span>{title}</span>
+      <span className="font-mono captialize cursoer">{title}</span>
     </div>
   );
 };
