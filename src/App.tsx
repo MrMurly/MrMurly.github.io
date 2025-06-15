89e9880578d29397a5320cca9765588217fca43b
Gustav Melefors <gusme799@student.liu.se>
2025-06-15 19:20:10 +0200

    feat: removed footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import { Contact } from "./components/Contact";
-import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { ProjectView } from "./components/ProjectView";
 import { Welcome } from "./components/Welcome";
@@ -14,7 +13,6 @@ function App() {
         <ProjectView />
         <Contact />
       </div>
-      <Footer />
     </div>
   );
 }
